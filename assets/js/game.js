5f47736f758631fea1ec9e4a0f037e05fb707f0e
luisgcodes <lcienfuegos95@gmail.com>
2022-03-03 22:48:17 -0800

    localStorage

--- assets/js/game.js
+++ assets/js/game.js
@@ -169,20 +169,23 @@ var endGame = function() {
   window.alert("The game has now ended. Let's see how you did!");
 
   // check localStorage for high score, if it's not there, use 0
-  var highScore = localStorage.getItem("highscore");
-  if (highScore === null) {
+    var highScore = localStorage.getItem("highscore");
+    if (highScore === null) {
     highScore = 0;
-  }
+    }
 
-  // if player has more money than the high score, player has new high score!
-  if (playerInfo.money > highScore) {
-    localStorage.setItem("highscore", playerInfo.money);
-    localStorage.setItem("name", playerInfo.name);
+  
+    // if player has more money than the high score, player has new   high score!
+  
+      if (playerInfo.money > highScore) {
+      localStorage.setItem("highscore", playerInfo.money);
+      localStorage.setItem("name", playerInfo.name);
 
-    alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
-  } else {
-    alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
-  }
+      
+      alert(playerInfo.name + " now has the high score of " + playerInfo.money + "!");
+    } else {
+      alert(playerInfo.name + " did not beat the high score of " + highScore + ". Maybe next time!");
+    }
 
   // ask player if they'd like to play again
   var playAgainConfirm = window.confirm("Would you like to play again?");
